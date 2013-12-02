2894c6559464e977243eda8cf626aae124fdbc4b
maxaon <maxaon@xakep.ru>
2013-12-02 18:04:36 +0200

    + public attribute for json view

--- djangular/middleware.py
+++ djangular/middleware.py
@@ -1,4 +1,3 @@
-
 class AngularJsonVulnerabilityMiddleware(object):
     """
     A middleware that inserts the AngularJS JSON Vulnerability request on JSON responses.
@@ -8,10 +7,11 @@ class AngularJsonVulnerabilityMiddleware(object):
 
     # Make this class easy to extend by allowing class level access.
     VALID_STATUS_CODES = [200, 201, 202]
-    VALID_CONTENT_TYPES = ['application/json']
+    VALID_CONTENT_TYPES = ['application/json', 'text/javascript']
 
     def process_response(self, request, response):
         if response.status_code in self.VALID_STATUS_CODES and response['Content-Type'] in self.VALID_CONTENT_TYPES:
-            response.content = self.CONTENT_PREFIX + response.content
+            if not (getattr(response, 'is_public', False) is True or "admin/jsi18n" in request.path):
+                response.content = self.CONTENT_PREFIX + response.content
 
         return response
